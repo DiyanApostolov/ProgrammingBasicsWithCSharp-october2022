9fa72944d1ed06692da427de6ed3ad9213f12633
KrIsKa7a <kristiqn.ivanov@abv.bg>
2022-10-02 13:30:35 +0300

    Upload solutions for exercise "First Steps In Coding".

---
+++ 02.FirstStepsInCoding-Exercise/P03.DepositCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P03.DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            double depositValue = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double interestPercantage = double.Parse(Console.ReadLine()) / 100;
+
+            //Actions
+            double monthGain = (depositValue * interestPercantage) / 12;
+            double depositGain = months * monthGain;
+            double totalSum = depositValue + depositGain;
+
+            //Output
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ 02.FirstStepsInCoding-Exercise/P05.VacationsBookList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P05.VacationsBookList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int pagesCount = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int daysCount = int.Parse(Console.ReadLine());
+
+            //Act
+            int hoursNeeded = pagesCount / pagesPerHour;
+            int hoursPerDay = hoursNeeded / daysCount;
+
+            //Output
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
