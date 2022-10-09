cc4df438136ad93e734b194561016d9ef0744f64
KrIsKa7a <kristiqn.ivanov@abv.bg>
2022-10-09 13:14:00 +0300

    Upload solutions for 04. Conditional Statements - Exercise

---
+++ 04.ConditionalStatements-Exercise/P03.TimeAfter15Minutes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P03.TimeAfter15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            //Action
+            minutes += 15; //After 15 minutes
+            if (minutes >= 60)
+            {
+                //Take us to the next hour
+                hour += 1;
+                minutes -= 60;
+            }
+
+            if (hour >= 24)
+            {
+                //Take us to the next day
+                hour -= 24;
+            }
+
+            //Output
+            //if (minutes <= 9)
+            //{
+            //    Console.WriteLine($"{hour}:0{minutes}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{hour}:{minutes}");
+            //}
+            Console.WriteLine($"{hour}:{minutes:d2}");
+        }
+    }
+}
---
+++ 04.ConditionalStatements-Exercise/P04.ToyShop/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P04.ToyShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            //Cnt = count
+            //Num = number
+            double tripPrice = double.Parse(Console.ReadLine());
+            int puzzlesCnt = int.Parse(Console.ReadLine());
+            int dollsCnt = int.Parse(Console.ReadLine());
+            int bearsCnt = int.Parse(Console.ReadLine());
+            int minionsCnt = int.Parse(Console.ReadLine());
+            int trucksCnt = int.Parse(Console.ReadLine());
+
+            //Actions
+            //In practice -> When working with money we use decimal
+            double puzzlesPrice = puzzlesCnt * 2.60;
+            double dollsPrice = dollsCnt * 3;
+            double bearsPrice = bearsCnt * 4.10;
+            double minionsPrice = minionsCnt * 8.20;
+            double trucksPrice = trucksCnt * 2;
+
+            double allToysCnt = puzzlesCnt + dollsCnt + bearsCnt 
+                + minionsCnt + trucksCnt;
+            double totalPrice = puzzlesPrice + dollsPrice + bearsPrice 
+                + minionsPrice + trucksPrice;
+
+            if (allToysCnt >= 50)
+            {
+                //Price - 25% => Price - (25% from Price) = Price - (0.25 * Price)
+                //totalPrice = totalPrice * 0.75;
+                //totalPrice *= 0.75;
+                double discount = 0.25 * totalPrice;
+                totalPrice -= discount;
+            }
+
+            double rent = 0.10 * totalPrice;
+            totalPrice -= rent;
+
+            //Round(0.1, 2) -> 0.1 <=> 0.10 <=> 1*10^-1 (Rounding -> Arithmetic operations)
+            //Formatting(0.1, 2) (0.1:f2) -> 0.10 </=> 0.1 (Formatting -> Text representation)
+            //Output
+            //double diff = totalPrice - tripPrice;
+            if (totalPrice >= tripPrice)
+            {
+                //Enough money
+                double moneyLeft = totalPrice - tripPrice;
+                Console.WriteLine($"Yes! {moneyLeft:f2} lv left.");
+            }
+            else
+            {
+                //Insufficent funds
+                double moneyNeeded = tripPrice - totalPrice;
+                Console.WriteLine($"Not enough money! {moneyNeeded:f2} lv needed.");
+            }
+        }
+    }
+}
