26a96f1e38ce19bde1ce6a3f0a19259e20d087ef
Diyan Apostolov <diyan.apostolov@yahoo.com>
2022-10-22 13:15:58 +0300

    Add homework - For loops

---
+++ 01.NumbersFrom1To100/04.EvenPowersOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
