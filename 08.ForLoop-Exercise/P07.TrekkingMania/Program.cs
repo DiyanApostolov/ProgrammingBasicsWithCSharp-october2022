527f239f5a50eca92a2228583f7e0cf12dd221bc
Diyan Apostolov <diyan.apostolov@yahoo.com>
2022-10-29 13:31:49 +0300

    Add 7 and 8 problem from For Loop - exercise

---
+++ 08.ForLoop-Exercise/P07.TrekkingMania/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace P07.TrekkingMania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfClimberGroups = int.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (int i = 1; i <= numberOfClimberGroups; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num <= 5)
+                {
+                    p1 += num;
+                }
+                else if (num >= 6 && num <= 12)
+                {
+                    p2 += num;
+                }
+                else if (num >= 13 && num <= 25)
+                {
+                    p3 += num;
+                }
+                else if (num >= 26 && num <= 40)
+                {
+                    p4 += num;
+                }
+                else if (num >= 41)
+                {
+                    p5 += num;
+                }
+            }
+
+            double allCount = p1 + p2 + p3 + p4 + p5;
+            p1 = p1 / allCount * 100;
+            p2 = p2 / allCount * 100;
+            p3 = p3 / allCount * 100;
+            p4 = p4 / allCount * 100;
+            p5 = p5 / allCount * 100;
+
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+            Console.WriteLine($"{p4:f2}%");
+            Console.WriteLine($"{p5:f2}%");
+        }
+    }
+}
---
+++ 08.ForLoop-Exercise/P08.TennisRanklist/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace P08.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Четем от конзолата:
+            //=> Брой турнири, в които е участвал – цяло число в интервала [1…20]
+            int n = int.Parse(Console.ReadLine());
+
+            //=> Начален брой точки в ранглистата - цяло число в интервала[1...4000]
+            int points = int.Parse(Console.ReadLine());
+
+            //2. Създаваме променлива за:
+            //=> позицията на завършване:
+            string possition;
+
+            //=> общият брой точки:
+            int matchPoints = 0;
+
+            int totalSum = 0;
+
+            double wins = 0;
+
+            //Създаваме цикъл, който се върти от 1 до n включително
+            for (int i = 1; i <= n; i++)
+            {
+                possition = Console.ReadLine();
+
+                if (possition == "W")
+                {
+                    matchPoints += 2000;
+                    wins++;
+                }
+                else if (possition == "F")
+                {
+                    matchPoints += 1200;
+                }
+                else
+                {
+                    matchPoints += 720;
+                }
+
+                totalSum = points + matchPoints;
+            }
+
+            double averigePoints = matchPoints / n;
+
+            double percentWinsPerTournament = wins / n * 100;
+
+            Console.WriteLine($"Final points: {totalSum}");
+            Console.WriteLine($"Average points: {averigePoints}");
+            Console.WriteLine($"{percentWinsPerTournament:f2}%");
+        }
+    }
+}
