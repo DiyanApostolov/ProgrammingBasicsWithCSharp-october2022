09da3dd9887bdc72d2dbbabfc3668cde7f6783ca
Diyan Apostolov <diyan.apostolov@yahoo.com>
2022-11-02 14:13:28 +0200

    Add solution for 10. Odd Even Sum

---
+++ 07.ForLoop-Lab/10.OddEvenSum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Transactions;
+using System.Xml;
+
+namespace Zadachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += inputNumber;
+                }
+                else
+                {
+                    oddSum += inputNumber;
+                }
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(evenSum - oddSum)}");
+            }
+        }
+    }
+}
